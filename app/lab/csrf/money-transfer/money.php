74f62bd6d1667078493d3472a0144d754de54257
marc17b <159477447+marc17b@users.noreply.github.com>
2024-02-25 23:06:35 +0000

    csfr3.3

--- app/lab/csrf/money-transfer/money.php
+++ app/lab/csrf/money-transfer/money.php
@@ -1,20 +1,33 @@
 <?php
-    ob_start();
-    session_start();
-    
-    $_SESSION['authority'] = "user";
-    
+ob_start();
+session_start();
+
+try {
+    if (!isset($_SESSION['authority'])) {
+        // Redirigir si la sesión no está configurada
+        header("Location: login.php");
+        exit;
+    }
+
+    $authority = $_SESSION['authority'];
+
     $db = new PDO('sqlite:database.db');
     
     require("../../../lang/lang.php");
     $strings = tr();
     
-    $selectUser = $db -> prepare("SELECT * FROM csrf_money_transfer WHERE authority=:authority");
-    $selectUser -> execute(array('authority' => $_SESSION['authority']));
-    $selectUser_Info = $selectUser -> fetch();
-    
-    sleep(1);
-    
-    echo $strings['card_money']."  <b>" .$selectUser_Info['money']. " ".$strings['card_money_symbol']. "</b>";
+    $selectUser = $db->prepare("SELECT * FROM csrf_money_transfer WHERE authority=:authority");
+    $selectUser->execute(array('authority' => $authority));
+    $selectUser_Info = $selectUser->fetch();
 
+    sleep(1);
+
+    echo $strings['card_money'] . "  <b>" . $selectUser_Info['money'] . " " . $strings['card_money_symbol'] . "</b>";
+} catch (PDOException $e) {
+    // Manejar excepciones de la base de datos, si es necesario
+    echo "Error de la base de datos: " . $e->getMessage();
+} catch (Exception $e) {
+    // Manejar otras excepciones, si es necesario
+    echo "Error: " . $e->getMessage();
+}
 ?>
